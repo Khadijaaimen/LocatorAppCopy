a5062548a1400d75d1f80b7eb7636f3eca25c7a5
Khadijaaimen <58134039+Khadijaaimen@users.noreply.github.com>
2022-02-08 15:54:11 +0500

    Logout functionality added in UserLatLong

--- app/src/main/java/com/example/latlong/activities/UserLatLong.java
+++ app/src/main/java/com/example/latlong/activities/UserLatLong.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.latlong.GpsTracker;
 import com.example.latlong.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserLatLong extends AppCompatActivity {
 
     private GpsTracker gpsTracker;
     private TextView tvLatitude,tvLongitude,tvLatitude2,tvLongitude2;
+    private Button logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,16 @@ public class UserLatLong extends AppCompatActivity {
         tvLongitude = (TextView)findViewById(R.id.longitude);
         tvLatitude2 = (TextView)findViewById(R.id.latitude2);
         tvLongitude2 = (TextView)findViewById(R.id.longitude2);
+        logout = findViewById(R.id.logoutButton);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(UserLatLong.this, MainActivity.class));
+                finish();
+            }
+        });
 
         try {
             if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
